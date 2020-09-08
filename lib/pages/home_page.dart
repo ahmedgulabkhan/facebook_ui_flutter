e7b47aa57fd828ccbbfa8f44c3ed1477cb3023d8
demoncommand97 <demoncommand97@gmail.com>
2020-09-08 19:03:41 +0530

    :art: update homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,7 +5,29 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('facebook', style: TextStyle(color: Colors.blueAccent, fontSize: 25.0)),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text('facebook', style: TextStyle(color: Colors.blueAccent, fontSize: 27.0, fontWeight: FontWeight.bold)),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Icon(Icons.search, color: Colors.black),
+
+                SizedBox(width: 10.0),
+
+                Icon(Icons.message, color: Colors.black)
+              ]
+            ),
+          ],
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
       ),
       body: Center(
         child: Text('Welcome to Facebook'),
