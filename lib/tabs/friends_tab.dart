39172607a0753ca1351fe8b94093bb10fb7f1e8e
demoncommand97 <demoncommand97@gmail.com>
2020-09-11 22:46:26 +0530

    :art: friends_tab update

--- lib/tabs/friends_tab.dart
+++ lib/tabs/friends_tab.dart
@@ -4,7 +4,7 @@ class FriendsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-          child: Container(
+      child: Container(
         padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -374,7 +374,257 @@ class FriendsTab extends StatelessWidget {
                   ],
                 )
               ],
-            )
+            ),
+
+            Divider(height: 30.0),
+
+            Text('People You May Know', style: TextStyle(fontSize: 21.0, fontWeight: FontWeight.bold)),
+
+            SizedBox(height: 20.0),
+
+            Row(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.yellow,
+                  radius: 40.0,
+                ),
+                SizedBox(width: 20.0),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Jack Tyler', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 15.0),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          child: Text('Confirm', style: TextStyle(color: Colors.white, fontSize: 15.0)),
+                        ),
+                        SizedBox(width: 10.0),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          child: Text('Delete', style: TextStyle(color: Colors.black, fontSize: 15.0)),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+
+            SizedBox(height: 20.0),
+
+            Row(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.yellow,
+                  radius: 40.0,
+                ),
+                SizedBox(width: 20.0),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Jack Tyler', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 15.0),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          child: Text('Confirm', style: TextStyle(color: Colors.white, fontSize: 15.0)),
+                        ),
+                        SizedBox(width: 10.0),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          child: Text('Delete', style: TextStyle(color: Colors.black, fontSize: 15.0)),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+
+            SizedBox(height: 20.0),
+
+            Row(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.yellow,
+                  radius: 40.0,
+                ),
+                SizedBox(width: 20.0),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Jack Tyler', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 15.0),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          child: Text('Confirm', style: TextStyle(color: Colors.white, fontSize: 15.0)),
+                        ),
+                        SizedBox(width: 10.0),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          child: Text('Delete', style: TextStyle(color: Colors.black, fontSize: 15.0)),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+
+            SizedBox(height: 20.0),
+
+            Row(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.yellow,
+                  radius: 40.0,
+                ),
+                SizedBox(width: 20.0),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Jack Tyler', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 15.0),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          child: Text('Confirm', style: TextStyle(color: Colors.white, fontSize: 15.0)),
+                        ),
+                        SizedBox(width: 10.0),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          child: Text('Delete', style: TextStyle(color: Colors.black, fontSize: 15.0)),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+
+            SizedBox(height: 20.0),
+
+            Row(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.yellow,
+                  radius: 40.0,
+                ),
+                SizedBox(width: 20.0),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Jack Tyler', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 15.0),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          child: Text('Confirm', style: TextStyle(color: Colors.white, fontSize: 15.0)),
+                        ),
+                        SizedBox(width: 10.0),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          child: Text('Delete', style: TextStyle(color: Colors.black, fontSize: 15.0)),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
+
+            SizedBox(height: 20.0),
+
+            Row(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.yellow,
+                  radius: 40.0,
+                ),
+                SizedBox(width: 20.0),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Jack Tyler', style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold)),
+                    SizedBox(height: 15.0),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          child: Text('Confirm', style: TextStyle(color: Colors.white, fontSize: 15.0)),
+                        ),
+                        SizedBox(width: 10.0),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 35.0, vertical: 10.0),
+                          decoration: BoxDecoration(
+                            color: Colors.grey[300],
+                            borderRadius: BorderRadius.circular(5.0)
+                          ),
+                          child: Text('Delete', style: TextStyle(color: Colors.black, fontSize: 15.0)),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
           ],
         )
       ),
